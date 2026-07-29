ff5dba2e02671bef20aa2390501199dfef45bb1d
alexstimilli <alex.stimilli@gmail.com>
2026-07-29 15:49:11 +0200

    Add files via upload

--- birroo/vite.config.ts
+++ birroo/vite.config.ts
@@ -23,17 +23,17 @@ export default defineConfig(({mode}) => {
           scope: '/',
           icons: [
             {
-              src: '/birroo-icon-192.png',
+              src: '/icon-192.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: '/birroo-icon-512.png',
+              src: '/icon-512.png',
               sizes: '512x512',
               type: 'image/png'
             },
             {
-              src: '/birroo-icon-512.png',
+              src: '/icon-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable'
